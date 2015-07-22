9b88f186a952eae4c3f833678814623df3239b14
RXY0805 <RXY0805@HOTMAIL.COM>
2015-07-22 20:18:01 +1000

    deploy to appharbour
    0001

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,57 @@
+namespace FIFA14.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using FIFA14.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FIFA14.DAL.FIFA14Context>
+    {
+        public Configuration()
+        {
+            //AutomaticMigrationsEnabled = true;
+            //AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(FIFA14.DAL.FIFA14Context context)
+        {
+            try
+            {
+                GetGroups().ForEach(g => context.Groups.Add(g));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        private static List<Group> GetGroups()
+        {
+            return new List<Group>
+            {
+                new Group
+                {
+                    Title="A"
+                },
+                new Group
+                {
+                    Title="B"
+                },
+                new Group
+                {
+                    Title="C"
+                },
+                 new Group
+                {
+                    Title="D"
+                },
+                 new Group
+                {
+                    Title="E"
+                }
+            };
+        }
+    }
+}
